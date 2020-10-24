2d4f16b6f3caee47e03ca3fc0c37916fec3415ed
KristynaBartonova <kikisbartonova@gmail.com>
2020-10-24 23:20:08 +0200

    zmena2

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 
 let natahu = 'circle';
 let hraje = document.querySelector('.hra');
-const tlacitko = document.querySelectorAll('button');
+let tlacitko = document.querySelectorAll('button');
 
 for (let i = 0; i < tlacitko.length; i++) {
   tlacitko[i].addEventListener('click', (event) => {
-    if (natahu === 'circle') {
-      event.target.classList.toggle('board_field--circle');
-      hraje.innerHTML = `hraje:
-      <img class="ikonka" src="obrazek/cross.svg" alt="křížek" />`;
-      natahu = 'cross';
-      event.target.setAttribute('disabled', true);
-    } else if (natahu === 'cross') {
-      event.target.classList.toggle('board_field--cross');
-      hraje.innerHTML = ` hraje:
-      <img class="ikonka" src="obrazek/circle.svg" alt="kolečko" />`;
-     natahu = 'circle';
-      event.target.setAttribute('disabled', true);
+
+    switch(natahu){
+      case 'circle' : 
+        event.target.classList.toggle('board_field--circle');
+        hraje.innerHTML = `hraje:
+        <img class="ikonka" src="obrazek/cross.svg" alt="křížek" />`;
+        tlacitko[i].style.backgroundImage = "url('obrazek/circle.svg')";
+        tlacitko[i].style.backgroundRepeat = "no-repeat";
+        tlacitko[i].style.backgroundSize = "90px 90px";
+        tlacitko[i].style.backgroundPosition = "center";
+        event.target.setAttribute('disabled', true);
+        natahu = 'cross';
+        break;
+      case 'cross':
+        event.target.classList.toggle('board_field--cross');
+        hraje.innerHTML = ` hraje:
+        <img class="ikonka" src="obrazek/circle.svg" alt="kolečko" />`;
+        tlacitko[i].style.backgroundImage = "url('obrazek/cross.svg')";
+        tlacitko[i].style.backgroundRepeat = "no-repeat";
+        tlacitko[i].style.backgroundSize = "90px 90px";
+        tlacitko[i].style.backgroundPosition = "center";
+        event.target.setAttribute('disabled', true);
+        natahu = 'circle'; 
+        break;
     }
-  });
-}
+  });}
 
 
 
